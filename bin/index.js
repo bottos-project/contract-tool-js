32758adfb96ada5061053b026e1e5189f9eea421
yuanjunliang <1610712512@qq.com>
2018-11-24 10:20:43 +0800

    update download project

--- bin/index.js
+++ bin/index.js
@@ -8,7 +8,7 @@ program.version('1.0.0', '-v, --version')
        .command('init <name>')
        .action((name) => {
           spinner.start()
-          download('direct:https://github.com/bottos-project/contract-tool-js.git#master', 'test/tmp', {clone: true}, (err) => {
+          download('https://github.com/bottos-project/contract-tool-js.git', name, {clone: true}, (err) => {
               if(err){
                 spinner.fail('create project failed')
               }else{
